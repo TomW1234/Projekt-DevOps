240a7836d86fa011da9f57e3d634d38b6d669afb
TomW1234 <148466093+TomW1234@users.noreply.github.com>
2026-04-07 12:39:28 +0200

    Refactor main.py to use functions for addition

--- app/main.py
+++ app/main.py
@@ -3,12 +3,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-a = float(os.getenv('A', '0'))
-b = float(os.getenv('B', '0'))
+def add(a, b):
+    return a + b
 
-result = a + b
+def get_values_from_env():
+    a = float(os.getenv("A", "0"))
+    b = float(os.getenv("B", "0"))
+    return a, b
 
-if result.is_integer():
-    result = int(result)
+if __name__ == "__main__":
+    a, b = get_values_from_env()
+    result = add(a, b)
 
-print(result)
+    if result.is_integer():
+        result = int(result)
+
+    print(result)
